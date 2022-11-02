4fe4bff6728f622bff45c650d2719b6da29802cf
erltor <erltor@acos.no>
2022-11-02 14:08:55 +0100

    [WFH-17428]: update schemas

--- KS.Fiks.Protokoller.V1/Models/Feilmelding/FeilmeldingType.cs
+++ KS.Fiks.Protokoller.V1/Models/Feilmelding/FeilmeldingType.cs
@@ -1,9 +1,4 @@
 namespace KS.Fiks.Protokoller.V1.Models.Feilmelding
 {
-    public static class FeilmeldingType
-    {
-        public const string Ugyldigforespørsel = "no.ks.fiks.kvittering.ugyldigforespoersel.v1";
-        public const string Serverfeil = "no.ks.fiks.kvittering.serverfeil.v1";
-        public const string Ikkefunnet = "no.ks.fiks.kvittering.ikkefunnet.v1";
-    }
+  
 }
